0fea150859a4d4e57ef17ab83c49f71d395886b0
Rob Hruska <robhruska@gmail.com>
2017-11-16 16:44:58 -0600

    Add/update database methods

--- CalendarToSlack/Sql/SqlDatabase.cs
+++ CalendarToSlack/Sql/SqlDatabase.cs
@@ -19,25 +19,73 @@ namespace CalendarToSlack.Sql
 
         public void Start()
         {
-            var filePath = Path.Combine(_dbDirectory, "somedb.db");
+            var filePath = Path.Combine(_dbDirectory, "somedb3.db");
             _connection = new SQLiteConnection($"Data Source={filePath}; Version=3");
             _connection.Open();
         }
 
         public void Bootstrap()
         {
-            _connection.Execute(@"CREATE TABLE IF NOT EXISTS UserInfo (EmailAddress varchar(50), SlackAuthToken varchar(50), DefaultStatusText varchar(100), DefaultStatusEmoji varchar(100))");
-            //_connection.Execute(@"INSERT INTO UserInfo (EmailAddress, SlackAuthToken, DefaultStatusText, DefaultStatusEmoji) values ('jordan.degner@hudl.com', '', 'test', ':mindblown:')");
+            _connection.Execute(@"
+                CREATE TABLE IF NOT EXISTS Users (
+                    EmailAddress VARCHAR(100) NOT NULL,
+                    SlackAuthToken VARCHAR(50) NOT NULL,
+                    DefaultStatusText VARCHAR(100),
+                    DefaultStatusEmoji VARCHAR(100),
+                    PRIMARY KEY (EmailAddress)
+                )");
+
+            _connection.Execute(@"
+                CREATE TABLE IF NOT EXISTS Statuses (
+                    EmailAddress VARCHAR(100) NOT NULL,
+                    Match VARCHAR(100) NOT NULL,
+                    Text VARCHAR(100),
+                    Emoji VARCHAR(102),
+                    FOREIGN KEY (EmailAddress) REFERENCES Users(EmailAddress) ON DELETE CASCADE
+                )");
+        }
+
+        // TODO caller should set status text / status emoji to defaults if a new user
+        public void CreateOrUpdateUser(SqlUser user)
+        {
+            if (GetUserByEmail(user.EmailAddress) == null)
+            {
+                _connection.Execute(@"
+                    INSERT INTO Users (EmailAddress, SlackAuthToken, DefaultStatusText, DefaultStatusEmoji)
+                    VALUES (@EmailAddress, @SlackAuthToken, @DefaultStatusText, @DefaultStatusEmoji)", user);
+                return;
+            }
+
+            // On update, don't update the status text/emoji, assume they've already set it to something they want
+            // If this is called for an existing user, it probably means they're re-authing the Slack integration,
+            // so we're only updating their SlackAuthToken.
+            _connection.Execute(@"
+                UPDATE Users SET
+                    SlackAuthToken=@SlackAuthToken
+                WHERE EmailAddress=@EmailAddress", user);
         }
 
         public List<SqlUser> GetUsers()
         {
-            return _connection.Query<SqlUser>("SELECT * FROM UserInfo").ToList();
+            return _connection.Query<SqlUser>("SELECT * FROM Users").ToList();
         }
 
         public SqlUser GetUserByEmail(string email)
         {
-            return _connection.Query<SqlUser>("SELECT * FROM UserInfo WHERE EmailAddress = @Email", new { Email = email }).FirstOrDefault();
+            return _connection.Query<SqlUser>("SELECT * FROM Users WHERE EmailAddress = @Email", new { Email = email }).FirstOrDefault();
+        }
+
+        public void SetStatus(SqlStatus status)
+        {
+            RemoveStatus(status);
+            _connection.Execute(@"
+                INSERT INTO Statuses (EmailAddress, Match, Text, Emoji)
+                VALUES (@EmailAddress, @Match, @Text, @Emoji)", status);
+        }
+
+        public void RemoveStatus(SqlStatus status)
+        {
+            _connection.Execute(@"DELETE FROM Statuses WHERE EmailAddress=@EmailAddress AND Match=@Match", status);
         }
 
         public void Dispose()
---
+++ CalendarToSlack/Sql/SqlStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace CalendarToSlack.Sql
+{
+    public class SqlStatus
+    {
+        public string EmailAddress { get; set; }
+        public string Match { get; set; }
+        public string Text { get; set; }
+        public string Emoji { get; set; }
+    }
+}
